99ea42b7f92ad9a4e1e17979d6987b09b9c5eeee
suchith9999 <suchith.nanded@gmail.com>
2025-03-18 13:19:05 +0530

    Update selenium_.py

--- selenium/selenium_.py
+++ selenium/selenium_.py
@@ -1,32 +1,36 @@
-# import time
-# from selenium import webdriver
-
-# driver = webdriver.Chrome('WebScraping/selenium/chromedriver.exe')  # Optional argument, if not specified will search path.
-# driver.get('http://www.google.com/')
-# time.sleep(5) # Let the user actually see something!
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# time.sleep(5) # Let the user actually see something!
-# driver.quit()
 
 
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'
 # path = 'chromedriver.exe'
 driver = webdriver.Chrome()
 driver.get(website)
 time.sleep(5)
-all_matches_button = driver.find_elements(By.XPATH,'//label[@analytics-event="All matches"]')
-all_matches_button[0].click()
-time.sleep(5)
+all_matches_button = driver.find_element(By.XPATH,'//label[@analytics-event="All matches"]')
+all_matches_button.click()
+time.sleep(2)
 
 matches = driver.find_elements(By.TAG_NAME, 'tr')
-for match in matches:
-    print(match.text)
+# for match in matches:
+#     print(match.text)
 
-time.sleep(5)
+date = []
+home_team = []
+score = []
+away_team = []
+
+for match in matches:
+    date.append(match.find_element(By.XPATH, './td[1]').text)
+    home_team.append(match.find_element(By.XPATH, './td[3]').text)
+    score.append(match.find_element(By.XPATH, './td[4]').text)
+    away_team.append(match.find_element(By.XPATH, './td[5]').text)
+
+df = pd.DataFrame({'Date': date, 'Home Team': home_team, 'Score': score, 'Away Team': away_team})
+df.to_csv('matches.csv', index=False)
+
+# time.sleep(5)
 driver.quit()
